17c44714248a338f15dd1159fba3e0ed9314024d
Stephen <s.prizio@hotmail.com>
2020-05-30 18:43:15 -0400

    adds extra data to MapDetails

--- src/main/java/com/r6overwatch/overwatchapi/facades/entities/games/MapFacade.java
+++ src/main/java/com/r6overwatch/overwatchapi/facades/entities/games/MapFacade.java
@@ -2,15 +2,23 @@ package com.r6overwatch.overwatchapi.facades.entities.games;
 
 import com.google.common.collect.Lists;
 import com.r6overwatch.overwatchapi.converters.entities.games.MapConverter;
+import com.r6overwatch.overwatchapi.converters.nonentities.MapDetailsConverter;
 import com.r6overwatch.overwatchapi.facades.entities.OverwatchFacade;
+import com.r6overwatch.overwatchapi.models.entities.games.Game;
+import com.r6overwatch.overwatchapi.models.entities.players.Squad;
+import com.r6overwatch.overwatchapi.models.entities.season.Season;
 import com.r6overwatch.overwatchapi.resources.entities.games.MapResource;
+import com.r6overwatch.overwatchapi.resources.nonentities.MapDetails;
+import com.r6overwatch.overwatchapi.services.entities.games.GameService;
 import com.r6overwatch.overwatchapi.services.entities.games.MapService;
+import com.r6overwatch.overwatchapi.services.entities.players.SquadService;
+import com.r6overwatch.overwatchapi.services.entities.season.SeasonService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Facade for {@link MapResource}
@@ -21,14 +29,59 @@ import java.util.Optional;
 @Component
 public class MapFacade implements OverwatchFacade<MapResource> {
 
+    @Resource(name = "gameService")
+    private GameService gameService;
+
     @Resource(name = "mapConverter")
     private MapConverter mapConverter;
+
+    @Resource(name = "mapDetailsConverter")
+    private MapDetailsConverter mapDetailsConverter;
 
     @Resource(name = "mapService")
     private MapService mapService;
 
+    @Resource(name = "seasonService")
+    private SeasonService seasonService;
+
+    @Resource(name = "squadService")
+    private SquadService squadService;
+
 
     //  METHODS
+
+    /**
+     * Obtains a list of {@link MapDetails} for the given squad id
+     *
+     * @param squadId id of {@link Squad}
+     * @param seasonId id of {@link Season}
+     * @return list of {@link MapDetails}, without {@link com.r6overwatch.overwatchapi.models.entities.games.Map}s that don't have games played
+     */
+    public List<MapDetails> findMapDetailsForSquad(Long squadId, Long seasonId) {
+
+        if (squadId != null) {
+            Optional<Squad> squad = this.squadService.find(squadId);
+            Optional<Season> season = this.seasonService.find(seasonId);
+
+            if (squad.isPresent() && season.isPresent()) {
+                List<Game> games = this.gameService.findGamesBySquadAndSeasonSortedByDate(squad.get(), season.get(), null);
+                List<com.r6overwatch.overwatchapi.models.entities.games.Map> maps = this.mapService.findAll();
+
+                if (CollectionUtils.isNotEmpty(games) && CollectionUtils.isNotEmpty(maps)) {
+                    return
+                            maps
+                                    .stream()
+                                    .map(map -> this.mapDetailsConverter.convert(games, map))
+                                    .filter(details -> details.getGamesPlayed() > 0L)
+                                    .sorted(Comparator.comparing(MapDetails::getWinPercentage).reversed())
+                                    .collect(Collectors.toList());
+                }
+            }
+         }
+
+        return new ArrayList<>();
+    }
+
 
     /**
      * Obtains a {@link MapResource} for the given map name
